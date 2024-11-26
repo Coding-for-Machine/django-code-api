b9a3577d7b3fc8809894ce132466a6b2b2fb7eef
Coding-for-Machine <codingformachine.gmail.com>
2024-11-26 20:29:20 +0500

    mycommit

--- app/testunit/urls.py
+++ app/testunit/urls.py
@@ -1,11 +1,12 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import UnittestproblemsViewSet, MemorytimeViewSet
+from .views import UnittestproblemsViewSet, MemorytimeViewSet, UnittestproblemsCreateAPIView
 
 router = DefaultRouter()
-router.register('unittestproblems', UnittestproblemsViewSet)
+router.register('test', UnittestproblemsViewSet)
 router.register('memorytime', MemorytimeViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),  # API endpointlar uchun
+    path('api/test/create/', UnittestproblemsCreateAPIView.as_view(), name='unittestproblems-create'),
 ]
--- app/testunit/views.py
+++ app/testunit/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.generics import CreateAPIView
 from .models import Unittestproblems, Memorytime
 from .serializers import UnittestproblemsSerializer, MemorytimeSerializer
 
@@ -6,6 +7,10 @@ class UnittestproblemsViewSet(viewsets.ModelViewSet):
     queryset = Unittestproblems.objects.all()
     serializer_class = UnittestproblemsSerializer
 
+class UnittestproblemsCreateAPIView(CreateAPIView):
+    queryset = Unittestproblems.objects.all()
+    serializer_class = UnittestproblemsSerializer
+    
 class MemorytimeViewSet(viewsets.ModelViewSet):
     queryset = Memorytime.objects.all()
     serializer_class = MemorytimeSerializer
